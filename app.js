d970d7903d51fbf72a065e3f67da49014262c7e2
Cody Stiffler <codystiffleraz@gmail.com>
2023-02-03 23:17:37 -0700

    adding facebook login

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const passportLocalMongoose = require("passport-local-mongoose")
 const e = require('express')
 const GoogleStrategy = require("passport-google-oauth20").Strategy
 const findOrCreate = require("mongoose-findorcreate")
+const FacebookStrategy = require("passport-facebook").Strategy
 
 
 const app = express()
@@ -67,6 +68,20 @@ passport.use(new GoogleStrategy({
   }
 ));
 
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+    // userProfileURL: 
+  },
+  function( accessToken, refreshToken, profile, cb) {
+
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 
 app.get("/", function(req,res){
     res.render("home")
@@ -82,6 +97,16 @@ app.get('/auth/google/secrets',
         successRedirect: '/secrets',
         failureRedirect: '/login'
 }));
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 
 app.get("/login", function(req,res){
     res.render("login")
